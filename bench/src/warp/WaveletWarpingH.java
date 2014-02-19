75fcacd52e9ad333efa6956068969f642e91ac22
Dave Hale <dhale@mines.edu>
2014-02-19 14:57:42 -0700

    IRWLS with H as the weighting filter

---
+++ bench/src/warp/WaveletWarpingH.java
@@ -0,0 +1,403 @@
+/****************************************************************************
+Copyright (c) 2014, Colorado School of Mines and others. All rights reserved.
+This program and accompanying materials are made available under the terms of
+the Common Public License - v1.0, which accompanies this distribution, and is
+available at http://www.eclipse.org/legal/cpl-v10.html
+****************************************************************************/
+package warp;
+
+import edu.mines.jtk.dsp.*;
+import edu.mines.jtk.lapack.*;
+import edu.mines.jtk.util.Check;
+import static edu.mines.jtk.dsp.Conv.*;
+import static edu.mines.jtk.util.ArrayMath.*;
+
+/**
+ * Estimates a wavelet from alignment by warping of sequences or images.
+ * The two sequences or images are assumed to have been convolved with the
+ * same wavelet. Warping of one sequence or image to align with the other will
+ * cause the wavelet to be stretched or squeezed, and this distortion enables
+ * us to estimate the wavelet.
+ * <p>
+ * For images, convolution with the wavelet is assumed to be in only the 1st
+ * dimension. For definiteness, this 1st dimension is assumed to be time in
+ * the documentation below.
+ *
+ * @author Dave Hale, Colorado School of Mines
+ * @version 2014.02.16
+ */
+public class WaveletWarpingH {
+
+  /**
+   * Sets the min-max range of times used to estimate wavelet.
+   * @param itmin minimum time, in samples.
+   * @param itmax maximum time, in samples.
+   */
+  public void setTimeRange(int itmin, int itmax) {
+    _itmin = itmin;
+    _itmax = itmax;
+  }
+
+  /**
+   * Sets the stability factor by which to scale zero-lag of correlations.
+   * A factor slightly greater than one may stabilize estimates of
+   * inverse wavelets A.
+   * @param sfac stability factor.
+   */
+  public void setStabilityFactor(double sfac) {
+    _sfac = sfac;
+  }
+
+  /**
+   * Returns inverse wavelet a estimated by warping one sequence to another.
+   * The sequences are related by warping such that f[t] ~ g[u[t]].
+   * The specified wavelet h is just the current best estimate of h.
+   * @param na number of samples in the inverse wavelet a.
+   * @param ka the sample index for a[0].
+   * @param nh number of samples in the inverse wavelet a.
+   * @param kh the sample index for a[0].
+   * @param h array of coefficients for the wavelet h.
+   * @param u array of samples for warping u[t].
+   * @param f array of samples for sequence f[t].
+   * @param g array of samples for sequence g[t]
+   * @return array of coefficients for the inverse wavelet a.
+   */
+  public float[] getInverseA(
+    int na, int ka, int nh, int kh, float[] h, 
+    float[] u, float[] f, float[] g)
+  {
+    int nt = u.length;
+    Check.argument(-na<ka,"-na<ka");
+    Check.argument(ka<=0,"ka<=0");
+
+    // Differences d for all lags of inverse wavelet a.
+    float[][] d = computeDifferences(na,ka,nh,kh,h,u,f,g);
+
+    // The matrix C and right-hand-side vector b, for Ca = b. For zero lag, we
+    // have a0 = a[-ka] = 1, so that only na-1 coefficients of a are unknown;
+    // the unknown coefficients are those corresponding to non-zero lags.
+    int ma = na-1;
+    DMatrix c = new DMatrix(ma,ma);
+    DMatrix b = new DMatrix(ma,1);
+    for (int ia=0,ic=0; ia<na; ++ia) {
+      if (ia==-ka) continue; // skip lag zero, because a0 = 1
+      for (int ja=0,jc=0; ja<na; ++ja) {
+        if (ja==-ka) continue; // skip lag zero, because a0 = 1
+        double cij = dot(d[ia],d[ja]);
+        c.set(ic,jc,cij);
+        ++jc;
+      }
+      c.set(ic,ic,c.get(ic,ic)*_sfac);
+      double bi = -dot(d[ia],d[-ka]);
+      b.set(ic,0,bi);
+      ++ic;
+    }
+    //System.out.println("c=\n"+c);
+    //System.out.println("b=\n"+b);
+
+    // Solve for inverse filter a using Cholesky decomposition of C.
+    DMatrixChd chd = new DMatrixChd(c);
+    DMatrix a = chd.solve(b);
+    float[] aa = new float[na];
+    for (int ia=0,ic=0; ia<na; ++ia) {
+      if (ia==-ka) {
+        aa[ia] = 1.0f; // lag 0, so a0 = 1
+      } else {
+        aa[ia] = (float)a.get(ic,0);
+        ++ic;
+      }
+    }
+    return aa;
+  }
+  public float[] getInverseA2(
+    int na, int ka, int nh, int kh, float[] h, 
+    float[] u, float[] f, float[] g)
+  {
+    int nt = u.length;
+    Check.argument(-na<ka,"-na<ka");
+    Check.argument(ka<=0,"ka<=0");
+
+    // Matrix Q = HSLG.
+    float[][] q = new float[na][];
+    for (int ia=0,lag=ka; ia<na; ++ia,++lag) {
+      float[] dg = delay(lag,g);
+      float[] ldg = applyL(u,dg);
+      float[] sldg = applyS(u,ldg);
+      q[ia] = applyH(nh,kh,h,sldg);
+    }
+
+    // The matrix C and right-hand-side vector b, for Ca = b.
+    DMatrix c = new DMatrix(na,na);
+    DMatrix b = new DMatrix(na,1);
+    for (int ia=0; ia<na; ++ia) {
+      for (int ja=0; ja<na; ++ja) {
+        double cij = dot(q[ia],q[ja]);
+        c.set(ia,ja,cij);
+      }
+      c.set(ia,ia,c.get(ia,ia)*_sfac);
+      double bi = dot(q[ia],f);
+      b.set(ia,0,bi);
+    }
+    //System.out.println("c=\n"+c);
+    //System.out.println("b=\n"+b);
+
+    // Solve for inverse filter a using Cholesky decomposition of C.
+    DMatrixChd chd = new DMatrixChd(c);
+    DMatrix a = chd.solve(b);
+    float[] aa = new float[na];
+    float asum = 0.0f;
+    for (int ia=0; ia<na; ++ia) {
+      aa[ia] = (float)a.get(ia,0);
+      asum += aa[ia]*aa[ia];
+    }
+    return mul(sqrt(na/asum),aa);
+  }
+
+  /**
+   * Estimates the wavelet h from the inverse wavelet a.
+   * @param na number of samples in the inverse wavelet a.
+   * @param ka the sample index for a[0].
+   * @param a array of coefficients for the inverse wavelet a.
+   * @param nh number of samples in the wavelet h.
+   * @param kh the sample index for h[0].
+   */
+  public float[] getWaveletH(int na, int ka, float[] a, int nh, int kh) {
+    float[] one = {1.0f};
+    float[] ca1 = new float[nh];
+    float[] caa = new float[nh];
+    xcor(na,ka,a,1,0,one,nh,kh,ca1);
+    xcor(na,ka,a,na,ka,a,nh, 0,caa);
+    caa[0] *= _sfac;
+    SymmetricToeplitzFMatrix stm = new SymmetricToeplitzFMatrix(caa);
+    return stm.solve(ca1);
+  }
+  public float[] getWaveletH(
+    float sfac, int na, int ka, float[] a, int nh, int kh)
+  {
+    float[] one = {1.0f};
+    float[] ca1 = new float[nh];
+    float[] caa = new float[nh];
+    xcor(na,ka,a,1,0,one,nh,kh,ca1);
+    xcor(na,ka,a,na,ka,a,nh, 0,caa);
+    caa[0] *= sfac;
+    SymmetricToeplitzFMatrix stm = new SymmetricToeplitzFMatrix(caa);
+    return stm.solve(ca1);
+  }
+  public float[] getWaveletH2(
+    int nh, int kh, int na, int ka, float[] a,
+    float[] u, float[] f, float[] g)
+  {
+    int nt = u.length;
+    float[] ga = applyA(na,ka,a,g);
+    float[] lga = applyL(u,ga);
+    float[] q = applyS(u,lga);
+    float[] cqf = new float[nh];
+    float[] cqq = new float[nh];
+    xcor(nt,0,q,nt,0,f,nh,kh,cqf);
+    xcor(nt,0,q,nt,0,q,nh, 0,cqq);
+    cqq[0] *= _sfac;
+    SymmetricToeplitzFMatrix stm = new SymmetricToeplitzFMatrix(cqq);
+    return stm.solve(cqf);
+  }
+
+  /**
+   * Applies the specified inverse wavelet A.
+   * @param na number of samples in the inverse wavelet a.
+   * @param ka the sample index for a[0].
+   * @param a array of coefficients for the inverse wavelet a.
+   * @param f array with input sequence f(t).
+   * @return array with filtered output sequence.
+   */
+  public float[] applyA(int na, int ka, float[] a, float[] f) {
+    return convolve(na,ka,a,f);
+  }
+
+  /**
+   * Applies the specified wavelet H.
+   * @param nh number of samples in the wavelet h.
+   * @param kh the sample index for h[0].
+   * @param h array of coefficients for the wavelet h.
+   * @param f array with input sequence f(t).
+   * @return array with filtered output sequence.
+   */
+  public float[] applyH(int nh, int kh, float[] h, float[] f) {
+    return convolve(nh,kh,h,f);
+  }
+
+  /**
+   * Applies the low-pass anti-alias filter L.
+   * If the specified warping includes squeezing, then this method attenuates
+   * high frequencies that could be aliased during warping.
+   * @param u array of warping times u(t).
+   * @param f array with input sequence f(t).
+   * @return array with filtered output sequence.
+   */
+  public float[] applyL(float[] u, float[] f) {
+    return aaf(RMAX,u,f);
+  }
+
+  /**
+   * Applies the warping operator S.
+   * Does not apply an anti-alias low-pass filter.
+   * @param u array of warping times u(t).
+   * @param f array with input sequence f(t).
+   * @return array with warped output sequence.
+   */
+  public float[] applyS(float[] u, float[] f) {
+    return warp(u,f);
+  }
+
+  /**
+   * Applies the composite linear operator HSLA.
+   * The sequence of operations is (1) convolution with the inverse wavelet a,
+   * (2) anti-alias filtering (if squeezing), (3) warping, and (4) convolution
+   * with the wavelet h.
+   * @param na number of samples in the inverse wavelet a.
+   * @param ka the sample index for a[0].
+   * @param a array of coefficients for the inverse wavelet a.
+   * @param nh number of samples in the wavelet h.
+   * @param kh the sample index for h[0].
+   * @param h array of coefficients for the wavelet h.
+   * @param u array[nt] of warping times u(t).
+   * @param f array[nt] with input sequence.
+   * @return array[nt] with output sequence.
+   */
+  public float[] applyHSLA(
+    int na, int ka, float[] a,
+    int nh, int kh, float[] h,
+    float[] u, float[] f) 
+  {
+    int nt = f.length;
+    float[] af = applyA(na,ka,a,f);
+    float[] laf = applyL(u,af);
+    float[] saf = applyS(u,laf);
+    float[] hsaf = applyH(nh,kh,h,saf);
+    return hsaf;
+  }
+
+  ///////////////////////////////////////////////////////////////////////////
+  // private
+
+  private static final float RMAX = 10.0f; // limits anti-alias filter
+  private static final SincInterp _si = 
+    SincInterp.fromErrorAndFrequency(0.01,0.40);
+
+  private double _sfac = 1.0;
+  private int _itmin = -1;
+  private int _itmax = -1;
+
+  /**
+   * Returns the array of differences D = H(SLG-F).
+   */
+  private float[][] computeDifferences(
+    int na, int ka, int nh, int kh, float[] h,
+    float[] u, float[] f, float[] g)
+  {
+    g = applyL(u,g);
+    int nt = u.length;
+    float[][] d = new float[na][];
+    for (int ia=0,lag=ka; ia<na; ++ia,++lag) {
+      float[] df = delay(lag,f);
+      float[] dg = delay(lag,g);
+      float[] sdg = applyS(u,dg);
+      float[] di = sub(sdg,df);
+      d[ia] = applyH(nh,kh,h,di);
+    }
+    return d;
+  }
+
+  private double dot(float[] x, float[] y) {
+    int nt = x.length;
+    int itlo = (_itmin>0)?_itmin:0;
+    int ithi = (_itmax>0)?_itmax:nt-1;
+    double sum = 0.0;
+    for (int it=itlo; it<=ithi; ++it) 
+      sum += x[it]*y[it];
+    return sum;
+  }
+
+  /**
+   * Returns the largest squeezing r(t) = u'(t) not greater than rmax.
+   * If less than or equal to one, then no squeezing is implied by u(t).
+   */
+  private float squeezing(float rmax, float[] u) {
+    int nt = u.length;
+    int itlo = max(1,_itmin);
+    int ithi = min(_itmax,nt-1);
+    float r = 0.0f;
+    for (int it=itlo; it<=ithi; ++it) {
+      float du = u[it]-u[it-1];
+      if (r<du)
+        r = du;
+    }
+    return min(r,rmax);
+  }
+
+  /**
+   * If necessary, applies an anti-alias filter to the sequence x(t).
+   * An anti-alias filter is necessary if the warping includes squeezing.
+   */
+  private float[] aaf(float rmax, float[] u, float[] x) {
+    int nt = u.length;
+    float r = squeezing(RMAX,u);
+    if (r>1.0) {
+      float[] y = new float[nt];
+      BandPassFilter aaf = new BandPassFilter(0.0,0.5/r,0.10/r,0.01);
+      aaf.apply(x,y);
+      return y;
+    } else {
+      return copy(x);
+    }
+  }
+
+  /**
+   * Returns y(t) = x(t-lag).
+   */
+  private static float[] delay(int lag, float[] x) {
+    int nt = x.length;
+    int itlo = max(0,lag);   // 0 <= it-lag
+    int ithi = min(nt,nt+lag); // it-lag < nt
+    float[] y = new float[nt];
+    for (int it=0; it<itlo; ++it)
+      y[it] = 0.0f;
+    for (int it=itlo; it<ithi; ++it)
+      y[it] = x[it-lag];
+    for (int it=ithi; it<nt; ++it)
+      y[it] = 0.0f;
+    return y;
+  }
+
+  /**
+   * Returns y(t) = x(u(t)).
+   */
+  private static float[] warp(float[] u, float[] x) {
+    int nt = u.length;
+    float[] y = new float[nt];
+    _si.interpolate(nt,1.0,0.0,x,nt,u,y);
+    y[0] *= u[1]-u[0];
+    for (int it=1; it<nt; ++it)
+      y[it] *= u[it]-u[it-1];
+    return y;
+  }
+
+  /**
+   * Returns y(t) = h(t)*x(t), where * denotes convolution.
+   */
+  private static float[] convolve(int nh, int kh, float[] h, float[] x) {
+    int nt = x.length;
+    float[] y = new float[nt];
+    convolve(nh,kh,h,x,y);
+    return y;
+  }
+  private static void convolve(
+    int nh, int kh, float[] h, float[] f,  float[] g)
+  {
+    int nt = f.length;
+    conv(nh,kh,h,nt,0,f,nt,0,g);
+  }
+
+  private float rms(float[] x) {
+    int nt = x.length;
+    return (float)sqrt(dot(x,x)/nt);
+  }
+}
---
+++ bench/src/warp/waveletwarpingh.py
@@ -0,0 +1,173 @@
+#############################################################################
+# Demo wavelet estimation from warping.
+# This version minimizes || f - HSLAg ||, using iteratively 
+# reweighted least-squares (IRWLS), where the inverse wavelet A
+# computed in one iteration is used to construct the wavelet H,
+# which is then used as the weighting filter in the next iteration.
+# f ~ HSLAg => HAf ~ HSLAg => HFa ~ HSLGa => H(F-SLG)a ~ 0
+
+from imports import *
+
+from edu.mines.jtk.dsp.Conv import *
+from warp import WaveletWarpingH
+
+#############################################################################
+
+#pngDir = "./png"
+pngDir = None
+
+def main(args):
+  goSimpleTest()
+
+def goSimpleTest():
+  nt,ni = 481,2 # number of time samples; number of impulses
+  freq,decay = 0.08,0.05 # peak frequency and decay for wavelet
+  na,ka = 81,-20 # sampling for inverse wavelet A
+  #na,ka = 5,-2 # sampling for inverse wavelet A
+  nh,kh = 181,-90 # sampling for wavelet H
+  dt,ft = 0.004,0.000 # used for plotting only
+  tmin,tmax = 0,nt-1
+  sfac = 1.000
+  st = Sampling(nt,dt,ft)
+  for mp in [True,False]: # True, for minimum-phase; False for other
+    hk = getWavelet(freq,decay,nh,kh,mp) # known wavelet
+    for r in [0.5]: # 0.5 for stretch; 2.0 for squeeze
+      aw = zerofloat(na); aw[-ka] = 1.0
+      hw = zerofloat(nh); hw[-kh] = 1.0
+      p,q = makeImpulses(r,nt,ni)
+      f = addWavelet(freq,decay,p,mp)
+      g = addWavelet(freq,decay,q,mp)
+      u = rampfloat(0.0,r,nt)
+      bpf = None
+      if r<=1.0:
+        u = add((1.0-r)*(nt-1),u)
+        bpf = BandPassFilter(0,0.5*r,0.05,0.01)
+      ww = WaveletWarpingH()
+      ww.setTimeRange(tmin,tmax)
+      ww.setStabilityFactor(sfac)
+      ak = ww.getWaveletH(nh,kh,hk,na,ka) # known inverse wavelet
+      #dump(ak)
+      for iter in range(2):
+        bpf.apply(hw,hw)
+        aw = ww.getInverseA(na,ka,nh,kh,hw,u,f,g) # estimated inverse
+        hw = ww.getWaveletH(na,ka,aw,nh,kh) # estimated wavelet
+        #dump(aw)
+      sg = ww.applyS(u,g)
+      hslag = ww.applyHSLA(na,ka,aw,nh,kh,hw,u,g)
+      nhw = normalize(hw)
+      nhk = normalize(hk)
+      title = "r = "+str(r)
+      #plotSequences(st,[f,g],labels=["f","g"],title=title)
+      #plotSequences(st,[f,sg],labels=["f","Sg"],title=title)
+      plotSequences(st,[f,g],labels=["f","g"],title=title)
+      plotSequences(st,[f,hslag],labels=["f","HSLAg"],title=title)
+      plotWavelets(Sampling(nh,dt,kh*dt),[nhw,nhk],title=title)
+
+def normalize(h):
+  #return div(h,max(max(h),-min(h)))
+  return div(h,rms(h));
+def rms(h):
+  return sqrt(sum(mul(h,h))/len(h))
+
+def makeImpulses(r,nt,ni):
+  p = zerofloat(nt)
+  q = zerofloat(nt)
+  tmax = nt-1
+  if r<=1.0:
+    ts = rampfloat(tmax/(ni+1),tmax/(ni+1),ni)
+  else:
+    ts = rampfloat(tmax/(ni+1)/r,tmax/(ni+1)/r,ni)
+  si = SincInterp.fromErrorAndFrequency(0.01,0.45)
+  rj = -1.0
+  for ji in range(ni):
+    tj = ts[ji]
+    tp = ts[ji]
+    tq = r*tp
+    if r<=1.0:
+      tq += (1.0-r)*tmax
+    #print "tp =",tp," tq =",tq
+    rj = -rj
+    si.accumulate(tp,rj,nt,1.0,0.0,p)
+    si.accumulate(tq,rj,nt,1.0,0.0,q)
+  return p,q
+
+def addWavelet(fpeak,decay,p,mp=False):
+  w = 2.0*PI*fpeak
+  if not mp:
+    decay *= 2.0
+    w -= 2.0*PI*0.04
+  r = exp(-decay)
+  a1,a2 = -2.0*r*cos(w),r*r
+  #print "a =",[1,a1,a2]
+  poles = [Cdouble.polar(r,w),Cdouble.polar(r,-w)]
+  zeros = []
+  gain = 1.0
+  x = copy(p)
+  t = copy(p)
+  rcf = RecursiveCascadeFilter(poles,zeros,gain)
+  rcf.applyForward(p,t)
+  if not mp:
+    w = 2.0*PI*(fpeak+0.04)
+    poles = [Cdouble.polar(r,w),Cdouble.polar(r,-w)]
+    zeros = []
+    gain = 1.0
+    rcf = RecursiveCascadeFilter(poles,zeros,gain)
+    rcf.applyReverse(t,x)
+  else:
+    copy(t,x)
+  conv(2,0,[1.0,-0.95],len(x),0,copy(x),len(x),0,x) # attenuate DC
+  return x
+
+def getWavelet(fpeak,decay,nh,kh,mp=False):
+  x = zerofloat(nh)
+  x[-kh] = 1.0
+  return addWavelet(fpeak,decay,x,mp)
+
+def plotSequence(x,xmax=None,title=None):
+  sp = SimplePlot.asPoints(x)
+  if xmax==None:
+    xmax = max(abs(max(x)),abs(min(x)))
+    xmax *= 1.05
+  sp.setVLimits(-xmax,xmax)
+  if title:
+    sp.setTitle(title)
+
+def plotSequences(st,xs,labels=None,title=None):
+  nx = len(xs)
+  pp = PlotPanel(nx,1)
+  for ix,xi in enumerate(xs):
+    pv = pp.addPoints(ix,0,st,xi)
+    if labels:
+      pp.setVLabel(ix,labels[ix])
+  pp.setHLabel("Time (s)")
+  pf = PlotFrame(pp)
+  pf.setVisible(True)
+  if title:
+    pp.setTitle(title)
+
+def plotWavelets(st,hs,hmax=None,title=None):
+  sp = SimplePlot()
+  ls = [PointsView.Line.SOLID,PointsView.Line.DASH,PointsView.Line.DOT]
+  nh = len(hs)
+  hsmax = 0
+  for ih in range(nh):
+    if hs[ih]:
+      pv = sp.addPoints(st,hs[ih])
+      pv.setLineStyle(ls[ih])
+      #pv.setLineWidth(2)
+      hsmax = max(hsmax,abs(max(hs[ih])),abs(min(hs[ih])))
+  if hmax==None:
+    hmax = hsmax*1.05
+  sp.setVLimits(-hmax,hmax)
+  sp.setVLabel("Amplitude (normalized)")
+  sp.setHLabel("Time (s)")
+  if title:
+    sp.setTitle(title)
+
+#############################################################################
+# Do everything on Swing thread.
+
+class RunMain(Runnable):
+  def run(self):
+    main(sys.argv)
+SwingUtilities.invokeLater(RunMain())
